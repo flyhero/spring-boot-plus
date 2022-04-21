4b52cea5815ca50197765c15d028c36e56bddc53
flyhero <qfwang666@163.com>
2022-04-21 22:26:32 +0800

    feat: 注册集合排序

--- src/main/java/io/github/flyhero/springboot/plus/generator/GeneratorRegistrar.java
+++ src/main/java/io/github/flyhero/springboot/plus/generator/GeneratorRegistrar.java
@@ -1,7 +1,13 @@
 package io.github.flyhero.springboot.plus.generator;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 生成器注册器
@@ -17,7 +23,14 @@ public final class GeneratorRegistrar {
         GENERATORS.add(generator);
     }
 
+    // 每次获取的时候都排序，性能不是很好
     public static List<AbstractGenerator> getGenerators() {
-        return GENERATORS;
+        return GENERATORS.stream().sorted(Comparator.comparing(v -> {
+            Order order = v.getClass().getAnnotation(Order.class);
+            if (Objects.isNull(order)){
+                return Ordered.HIGHEST_PRECEDENCE;
+            }
+           return order.value();
+        })).collect(Collectors.toList());
     }
 }
